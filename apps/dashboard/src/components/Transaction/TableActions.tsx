e1e1bc9b0c5ea8fd316b612ba2a3d39f19eae93b
dominggo1999 <arnold.ds.dev@gmail.com>
2024-01-18 16:22:06 +0000

    feat : connect delete action to api

--- apps/dashboard/src/components/Transaction/TableActions.tsx
+++ apps/dashboard/src/components/Transaction/TableActions.tsx
@@ -2,15 +2,46 @@ import { ActionIcon } from "@mantine/core";
 import { IconEye, IconTrash, IconPencil } from "@tabler/icons-react";
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { api } from "trpc";
+import { notifications } from "@mantine/notifications";
 
 interface TableActionsProps {
   id: string;
 }
 
 const TableActions: FC<TableActionsProps> = ({ id }) => {
+  const apiUtils = api.useUtils();
+
+  const { mutateAsync: deleteTransaction, isLoading } =
+    api.transaction.delete.useMutation({
+      onSuccess: () => {
+        notifications.show({
+          title: "Transaction Deleted",
+          message: "Transaction deleted successfully",
+          color: "green",
+        });
+        apiUtils.transaction.getAll.invalidate();
+      },
+      onError: () => {
+        notifications.show({
+          title: "Transaction Failed",
+          message: "Transaction failed to delete",
+          color: "red",
+        });
+      },
+    });
+
+  const handleDelete = async () => {
+    await deleteTransaction(id);
+  };
+
   return (
     <div className="flex gap-2">
-      <ActionIcon radius="xl" onClick={() => console.log(id)}>
+      <ActionIcon
+        loading={isLoading}
+        radius="xl"
+        onClick={() => handleDelete()}
+      >
         <IconTrash size={18} />
       </ActionIcon>
       <ActionIcon radius="xl" component={Link} to={`/transaction/${id}`}>
