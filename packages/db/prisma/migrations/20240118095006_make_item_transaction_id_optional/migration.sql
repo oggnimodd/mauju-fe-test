86297ed0b636afda0fdd83c27152188ea3eb76fb
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-18 09:50:47 +0000

    fix : make Item transactionId optional in prisma schema

---
+++ packages/db/prisma/migrations/20240118095006_make_item_transaction_id_optional/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Item" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "quantity" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "transactionId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Item_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Item" ("createdAt", "description", "id", "name", "price", "quantity", "transactionId", "updatedAt", "userId") SELECT "createdAt", "description", "id", "name", "price", "quantity", "transactionId", "updatedAt", "userId" FROM "Item";
+DROP TABLE "Item";
+ALTER TABLE "new_Item" RENAME TO "Item";
+CREATE UNIQUE INDEX "Item_id_key" ON "Item"("id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
