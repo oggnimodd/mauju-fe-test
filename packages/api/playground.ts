6fe550855256a39ff75504e0a7ebc2dd593ab424
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-18 09:10:18 +0000

    chore : upgrade elysia

--- packages/api/playground.ts
+++ packages/api/playground.ts
@@ -6,18 +6,18 @@ import { cors } from "@elysiajs/cors";
 import { createTRPCContext } from "./node/trpc";
 import { renderTrpcPanel } from "trpc-panel";
 import { staticPlugin } from "@elysiajs/static";
+import { html } from "@elysiajs/html";
 import { injectScriptToPanel } from "./panel/injectScriptToPanel";
 
 const PORT = 8080;
 
 const app = new Elysia();
 
-app.use(cors()).use(staticPlugin()).use(clerkPlugin());
+app.use(html()).use(cors()).use(staticPlugin()).use(clerkPlugin());
 
 // Only in development
 if (process.env.NODE_ENV === "development") {
-  app.get("/panel", ({ set }) => {
-    set.headers["Content-Type"] = "text/html";
+  app.get("/panel", () => {
     const panelHtml = renderTrpcPanel(appRouter, {
       url: process.env.VITE_APP_TRPC_URL || `http://localhost:${PORT}/trpc`,
       transformer: "superjson",
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "development") {
 
     return injectScriptToPanel({
       html: panelHtml,
-      staticPath: "public",
+      staticPath: "/public",
     });
   });
 }
