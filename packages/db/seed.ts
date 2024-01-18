9f7d1f9f06c05ed5c0a81e420d4b339205d5811f
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-18 10:45:32 +0000

    feat : add seed db script

--- packages/db/seed.ts
+++ packages/db/seed.ts
@@ -1,48 +1,63 @@
 // This script is only used in development to seed the database
-import { searchBooks } from "@acme/google-books";
 import { prisma } from "./index";
+import type { STATUS } from "../api/models";
+import { v4 as uuidv4 } from "uuid";
+import { Item } from "./index";
+
+const TRANSACTION_STATUS: STATUS[] = ["PENDING", "SUCCESS", "FAILED"];
 
 const USER_ID = process.env.SEED_USER_ID;
-const NUMBER_OF_BOOKS_PER_SHELF = 10;
-const QUERY = "Harry Potter";
 
 if (!USER_ID) {
   throw new Error("Please add USER_ID to .env file before running seed script");
 }
 
-// Get all shelf
-const shelf = await prisma.shelf.findMany({
-  where: {
-    userId: USER_ID,
-  },
-});
+// Define the number of transactions to seed
+const NUMBER_OF_TRANSACTIONS = 100;
 
-// Delete all books
-await prisma.book.deleteMany({
-  where: {
-    userId: USER_ID,
-  },
-});
+// Clear existing transactions
+await prisma.transaction.deleteMany();
 
-// Search random books
-const books = await searchBooks({
-  query: QUERY,
-  startIndex: 0,
-  maxResults: NUMBER_OF_BOOKS_PER_SHELF * shelf.length,
-});
+type BASED_ITEM = Omit<Item, "transactionId" | "createdAt" | "updatedAt">;
 
-if (books.items?.length) {
-  for (let i = 0; i < books.items?.length; i++) {
-    await prisma.book.create({
-      data: {
-        title: books.items[i].volumeInfo.title,
-        description: books.items[i].volumeInfo.description,
-        author: books.items[i].volumeInfo.authors?.join(","),
-        cover: books.items[i].volumeInfo.imageLinks?.thumbnail,
-        googleBooksUrl: books.items[i].volumeInfo.infoLink,
-        userId: USER_ID,
-        shelfId: shelf[i % shelf.length].id,
-      },
-    });
+const generateItems = (numItems: number) => {
+  const items: BASED_ITEM[] = [];
+
+  for (let i = 0; i < numItems; i++) {
+    const item: BASED_ITEM = {
+      name: `Item ${i + 1}`,
+      description: `Description for Item ${i + 1}`,
+      quantity: Math.floor(Math.random() * 10) + 1,
+      price: Math.round(Math.random() * 100),
+      userId: USER_ID,
+
+      id: uuidv4(),
+    };
+
+    items.push(item);
   }
+
+  return items;
+};
+
+for (let i = 0; i < NUMBER_OF_TRANSACTIONS; i++) {
+  const randomStatus =
+    TRANSACTION_STATUS[Math.floor(Math.random() * TRANSACTION_STATUS.length)];
+  const items = generateItems(Math.floor(Math.random() * 5) + 1); // Generate between 1 and 5 items
+  const totalPrice = items.reduce(
+    (total, item) => total + item.quantity * item.price,
+    0,
+  );
+
+  await prisma.transaction.create({
+    data: {
+      id: uuidv4(),
+      userId: USER_ID, // replace with actual user id
+      total: totalPrice,
+      status: randomStatus,
+      items: {
+        create: items,
+      },
+    },
+  });
 }
