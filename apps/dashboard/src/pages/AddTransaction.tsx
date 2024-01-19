89ca03471d5a06efee283fb51590c57a99fed01e
dominggo1999 <arnold.ds.dev@gmail.com>
2024-01-19 03:18:22 +0000

    feat: AddTransactionPage

--- apps/dashboard/src/pages/AddTransaction.tsx
+++ apps/dashboard/src/pages/AddTransaction.tsx
@@ -1,13 +1,42 @@
 import { BaseLayout } from "layouts";
-import { PageHeader } from "components";
+import { PageHeader, TransactionForm } from "components";
+import { useNavigate } from "react-router-dom";
+import { api } from "trpc";
+import { notifications } from "@mantine/notifications";
 
 const AddTransaction = () => {
+  const navigate = useNavigate();
+  const {
+    mutateAsync: createTransaction,
+    isLoading,
+    error,
+  } = api.transaction.create.useMutation({
+    onSuccess: () => {
+      notifications.show({
+        title: "Transaction Created",
+        message: "Transaction created successfully",
+        color: "green",
+      });
+      navigate("/");
+    },
+    onError: () => {
+      notifications.show({
+        title: "Transaction Failed",
+        message: "Transaction failed to create",
+        color: "red",
+      });
+    },
+  });
+
   return (
-    <div>
-      <BaseLayout>
-        <PageHeader title="Create Transaction" />
-      </BaseLayout>
-    </div>
+    <BaseLayout>
+      <PageHeader title="Create Transaction" />
+      <TransactionForm
+        onSubmit={createTransaction}
+        isLoading={isLoading}
+        errorMessage={error?.message}
+      />
+    </BaseLayout>
   );
 };
 
